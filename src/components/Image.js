7e1fa71d1f16a16c10808d59ed63a1f505c92f23
Mano <manoaway@gmail.com>
2020-09-26 18:27:31 +0200

    non-responsive mansory gallery

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,6 @@ import '../styles/style.min.css';
 const Image = ({ image, title, description}) =>{
 return(
   <figure className="figure">
-    <h3>{title}</h3>
     <img className="figure__img" src={image} alt={title}/>
     <figcaption>{description}</figcaption>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ const Navbar = ({ updateSection, section, sort, windowP, showViral, updateWindow
    {/*choose section to display*/}
   < fieldset className="gallery__section">
     <input type="radio" id="hot" name="section" value="hot" onClick={updateSection}/>
-    <label htmlFor="hot" >HOT</label><br/>
+    <label htmlFor="hot" >HOT</label>
     <input type="radio" id="top" name="section" value="top" onClick={updateSection}/>
-    <label htmlFor="top">TOP</label><br/>
+    <label htmlFor="top">TOP</label>
     <input type="radio" id="user" name="section" value="user" onClick={updateSection}/>
     <label htmlFor="user">USER</label>
   </ fieldset>
@@ -20,7 +20,7 @@ const Navbar = ({ updateSection, section, sort, windowP, showViral, updateWindow
 
 {/*display only if section is set to top*/}
     <div className={`form__window ${section === 'top' ? '' : 'display-none'}`}>
-        <label htmlFor="window" >Window Parameters
+        <label htmlFor="window" > Window Parameters  </label>
   <select value={windowP} onChange={updateWindowP}>
           <option value="day">Day</option>
           <option value="week">Week</option>
@@ -28,19 +28,19 @@ const Navbar = ({ updateSection, section, sort, windowP, showViral, updateWindow
           <option value="year">Year</option>
           <option value="all">All</option>
           </select>
-        </label>
+
   </div>
 
 {/*display only if section is set to user*/}
   <div className={`form__sort ${section === 'user' ? '' : 'display-none'}`}>
-  <label htmlFor="sort">Sort
+  <label htmlFor="sort">Sort  </label>
   <select value={sort} onChange={updateSort}>
           <option value="viral">Viral</option>
           <option value="top">Top</option>
           <option value="time">Time</option>
           <option value="rising">Rising</option>
           </select>
-  </label>
+
    </div>
 
 {/*toggle viral image display*/}
